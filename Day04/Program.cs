916d1e6e3bec4fd538c1553a3de0f8b5e2ad47fc
Ben Taylor <bentaylortheonly@gmail.com>
2017-12-05 15:46:03 +0000

    day 04 part 1 works

---
+++ Day04/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Day04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string line;
+            int valid = 0;
+            while (!string.IsNullOrEmpty(line = Console.ReadLine()))
+            {
+                if (IsValid(line))
+                {
+                    valid++;
+                }
+            }
+            Console.WriteLine(valid);
+        }
+
+        static bool IsValid(string line)
+        {
+            var words = Regex.Split(line.Trim(), @"\s+");
+            var isValid = words.Distinct().Count() == words.Length;
+            return isValid;
+        }
+    }
+}
