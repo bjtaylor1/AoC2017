0a91f2270a36d7a8458822800a9cd09f1305cbb1
Taylor <T23360@u-ssi.net>
2017-12-05 15:00:50 +0000

    day 03 part 2 works

--- Day03/Program.cs
+++ Day03/Program.cs
@@ -22,17 +22,20 @@ namespace Day03
             Point currentPoint = new Point(0, 0);
             Vector currentDir = new Vector(0);
             var pointNums = new Dictionary<Point, int> { { currentPoint, 1 } };
-            for (int i = 1; i < n; i++)
+            while(true)
             {
                 currentPoint = currentPoint + currentDir;
                 var num = pointNums.Where(k => k.Key.IsAdjacent(currentPoint)).Sum(k => k.Value);
+                if (num > n)
+                {
+                    return num;
+                }
                 pointNums.Add(currentPoint, num);
                 if (!pointNums.ContainsKey(currentPoint + currentDir.Left()))
                 {
                     currentDir = currentDir.Left();
                 }
             }
-            return pointNums.Last().Value;
         }
     }
 }
