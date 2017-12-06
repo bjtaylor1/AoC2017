8bdc738e9f61f871711fd3e7c9dd7c3beb94725b
Taylor <T23360@u-ssi.net>
2017-12-06 09:09:50 +0000

    day 04 part 2 works

--- Day04/Program.cs
+++ Day04/Program.cs
@@ -17,7 +17,12 @@ namespace Day04
             {
                 if (IsValid(line))
                 {
+                    Console.WriteLine("...valid");
                     valid++;
+                }
+                else
+                {
+                    Console.WriteLine("...invalid");
                 }
             }
             Console.WriteLine(valid);
@@ -26,8 +31,32 @@ namespace Day04
         static bool IsValid(string line)
         {
             var words = Regex.Split(line.Trim(), @"\s+");
-            var isValid = words.Distinct().Count() == words.Length;
+            var isValid = words.Distinct(AnagramEqualityComparer.Instance).Count() == words.Length;
             return isValid;
+        }
+    }
+
+    public class AnagramEqualityComparer : IEqualityComparer<string>
+    {
+        private AnagramEqualityComparer()
+        {
+            
+        }
+        public static AnagramEqualityComparer Instance = new AnagramEqualityComparer();
+        public bool Equals(string x, string y)
+        {
+            return string.Equals(Order(x), Order(y));
+        }
+
+        public int GetHashCode(string obj)
+        {
+            return Order(obj).GetHashCode();
+        }
+
+        private static string Order(string input)
+        {
+            var ordered = new string(input.ToCharArray().OrderBy(c => c).ToArray());
+            return ordered;
         }
     }
 }
