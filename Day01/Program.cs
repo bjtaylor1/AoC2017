6d89ea5ad2ff8977b39eb474d2a21e9cbfc7247c
Ben Taylor <bentaylortheonly@gmail.com>
2017-12-06 09:50:32 +0000

    days 1 and 2

---
+++ Day01/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input;
+            while((input = Console.ReadLine()) != null)
+            {
+                Process(input);
+            }
+        }
+
+        private static void Process(string input)
+        {
+            int[] digits = input.Select(i => int.Parse(i.ToString())).ToArray();
+            int[] digitsThatMatchNext = digits.Where((d, i) => d == digits[(i + (digits.Length / 2)) % digits.Length]).ToArray();
+            int sumOfDigitsThatMatchNext = digitsThatMatchNext.Sum();
+            Console.WriteLine(sumOfDigitsThatMatchNext);
+        }
+    }
+}
---
+++ Day02/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Day02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string line;
+            int tot = 0;
+            while(!string.IsNullOrEmpty(line = Console.ReadLine()))
+            {
+                string[] parts = Regex.Split(line.Trim(), @"\s+");
+                int[] numbers = parts.Select(int.Parse).ToArray();
+#if DAY2
+                int checksum = GetChecksum(numbers);
+#else
+                int checksum = numbers.Max() - numbers.Min();
+#endif
+                tot += checksum;
+            }
+            Console.WriteLine(tot);
+        }
+
+        static int GetChecksum(int[] numbers)
+        {
+            foreach(var number in numbers)
+            {
+                for(int factor = number * 2; factor <= numbers.Max(); factor += number)
+                {
+                    if(numbers.Contains(factor))
+                    {
+                        return factor / number;
+                    }
+                }
+            }
+            throw new InvalidOperationException("No divisable pairs!");
+        }
+    }
+}
