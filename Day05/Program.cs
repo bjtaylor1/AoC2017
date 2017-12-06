4bd05ea8eb2406f6a262ed54dfadb9002ecc44e2
Ben Taylor <bentaylortheonly@gmail.com>
2017-12-06 11:14:21 +0000

    day05 part 1 works

--- Day05/Program.cs
+++ Day05/Program.cs
@@ -10,6 +10,27 @@ namespace Day05
     {
         static void Main(string[] args)
         {
+            string line;
+            var instructionsList = new List<int>();
+            while (!string.IsNullOrEmpty(line = Console.ReadLine()))
+            {
+                instructionsList.Add(int.Parse(line.Trim()));
+            }
+
+            int currentInstruction = 0;
+            int steps = 0;
+            while (currentInstruction >= 0 && currentInstruction < instructionsList.Count)
+            {
+                var nextInstruction = instructionsList[currentInstruction];
+                instructionsList[currentInstruction]++;
+                currentInstruction += nextInstruction;
+                steps++;
+                //Console.WriteLine(string.Join(" ", instructionsList.Select((i,n) => n == currentInstruction ? $"({i})" : $" {i} ").ToArray()));
+            }
+
+            Console.WriteLine(steps);
         }
+
+        
     }
 }
