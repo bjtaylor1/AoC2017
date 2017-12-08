3757072fb31f3bbe20509b381dfa0351f6be4d8f
Ben Taylor <bentaylortheonly@gmail.com>
2017-12-08 09:45:01 +0000

    compiler services poc

--- Day08/Program.cs
+++ Day08/Program.cs
@@ -1,7 +1,9 @@
 ﻿using System;
+using System.CodeDom;
 using System.CodeDom.Compiler;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.CSharp;
@@ -13,25 +15,35 @@ namespace Day08
         static void Main(string[] args)
         {
             var csc = new CSharpCodeProvider(new Dictionary<string, string>());
-            var parameters = new CompilerParameters(new[] { "mscorlib.dll", "System.Core.dll" }, "day08dynamic.dll", true);
+
+            var parameters = new CompilerParameters(new[] { "mscorlib.dll", "System.Core.dll", Assembly.GetExecutingAssembly().Location }, "day08dynamic.dll", true);
+            
             var result = csc.CompileAssemblyFromSource(parameters,
-                @"public class Test
+                @"using Day08;
+                    public static class Test
                     {
-                        public static string GetMessage()
+                        public static Registers GetMessage()
                         {
-                            return ""Hello from dynamic!"";
+                            return new Registers(""Hello from dynamic!"");
                         }
                     }");
             if(result.NativeCompilerReturnValue != 0) throw new InvalidOperationException("Compiler error!");
 
             var testClass = result.CompiledAssembly.GetType("Test");
-            var testInst = Activator.CreateInstance(testClass);
-            var message = testClass.GetMethod("GetMessage")?.Invoke(testInst, new object[] {});
-
-            Console.Out.WriteLine(message);
+            var obj = testClass.GetMethod("GetMessage")?.Invoke(null, new object[] {});
+            var registers = (Registers) obj;
+            Console.Out.WriteLine(registers?.Message);
         }
+    }
 
+    public class Registers
+    {
+        public string Message { get; }
 
+        public Registers(string message)
+        {
+            Message = message;
+        }
     }
 
     
