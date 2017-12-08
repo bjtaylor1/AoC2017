dd704a31fb866fed91d54f01c1afdc878d51d0fd
Ben Taylor <bentaylortheonly@gmail.com>
2017-12-08 09:30:27 +0000

    compiler services poc

--- Day08/Program.cs
+++ Day08/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
+using System.CodeDom.Compiler;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.CSharp;
 
 namespace Day08
 {
@@ -10,8 +12,27 @@ namespace Day08
     {
         static void Main(string[] args)
         {
+            var csc = new CSharpCodeProvider(new Dictionary<string, string>());
+            var parameters = new CompilerParameters(new[] { "mscorlib.dll", "System.Core.dll" }, "day08dynamic.dll", true);
+            var result = csc.CompileAssemblyFromSource(parameters,
+                @"public class Test
+                    {
+                        public static string GetMessage()
+                        {
+                            return ""Hello from dynamic!"";
+                        }
+                    }");
+            if(result.NativeCompilerReturnValue != 0) throw new InvalidOperationException("Compiler error!");
+
+            var testClass = result.CompiledAssembly.GetType("Test");
+            var testInst = Activator.CreateInstance(testClass);
+            var message = testClass.GetMethod("GetMessage")?.Invoke(testInst, new object[] {});
+
+            Console.Out.WriteLine(message);
         }
 
 
     }
+
+    
 }
