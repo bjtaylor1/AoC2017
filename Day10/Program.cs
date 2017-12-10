e43f60451499edb31d1b6029277dc8507a513981
Ben Taylor <bentaylortheonly@gmail.com>
2017-12-10 12:11:43 +0000

    day10 part 1 works

--- Day10/Program.cs
+++ Day10/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Day10
 {
@@ -11,7 +8,28 @@ namespace Day10
         static void Main(string[] args)
         {
             string line;
-
+            int count = args.Length > 0 ? int.Parse(args[0]) : 256;
+            while(!string.IsNullOrEmpty(line = Console.ReadLine()))
+            {
+                int[] list = Enumerable.Range(0, count).ToArray();
+                int curPos = 0;
+                int skipSize = 0;
+                int[] lengths = line.Split(',').Select(s => int.Parse(s.Trim())).ToArray();
+                foreach(var length in lengths)
+                {
+                    int[] valuesToReverse = new int[length];
+                    for(var i = 0; i < length; i++)
+                    {
+                        valuesToReverse[i] = list[(curPos + i) % list.Length];
+                    }
+                    for(var i = 0; i < length; i++)
+                    {
+                        list[(curPos + i) % list.Length] = valuesToReverse[length - i - 1];
+                    }
+                    curPos += (length + skipSize++);
+                }
+                Console.WriteLine(list[0] * list[1]);
+            }
         }
     }
 }
