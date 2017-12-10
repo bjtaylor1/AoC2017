b596b8790651a5d375e16f5d0667d8651e0cd8a0
Ben Taylor <bentaylortheonly@gmail.com>
2017-12-10 12:47:09 +0000

    day 10 part 2 works

--- Day10/Program.cs
+++ Day10/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Linq;
+using System.Text;
 
 namespace Day10
 {
@@ -8,28 +9,39 @@ namespace Day10
         static void Main(string[] args)
         {
             string line;
+
             int count = args.Length > 0 ? int.Parse(args[0]) : 256;
             while(!string.IsNullOrEmpty(line = Console.ReadLine()))
             {
-                int[] list = Enumerable.Range(0, count).ToArray();
+                byte[] list = Enumerable.Range(0, count).Select(i => (byte)i).ToArray();
                 int curPos = 0;
                 int skipSize = 0;
-                int[] lengths = line.Split(',').Select(s => int.Parse(s.Trim())).ToArray();
-                foreach(var length in lengths)
+                int[] lengths = line.Trim().Select(c => (int)c).Concat(new[] { 17, 31, 73, 47, 23 }).ToArray();
+                for (int round = 0; round < 64; round++)
                 {
-                    int[] valuesToReverse = new int[length];
-                    for(var i = 0; i < length; i++)
+                    foreach (var length in lengths)
                     {
-                        valuesToReverse[i] = list[(curPos + i) % list.Length];
+                        byte[] valuesToReverse = new byte[length];
+                        for (var i = 0; i < length; i++)
+                        {
+                            valuesToReverse[i] = list[(curPos + i) % list.Length];
+                        }
+                        for (var i = 0; i < length; i++)
+                        {
+                            list[(curPos + i) % list.Length] = valuesToReverse[length - i - 1];
+                        }
+                        curPos += (length + skipSize++);
                     }
-                    for(var i = 0; i < length; i++)
-                    {
-                        list[(curPos + i) % list.Length] = valuesToReverse[length - i - 1];
-                    }
-                    curPos += (length + skipSize++);
                 }
-                Console.WriteLine(list[0] * list[1]);
+                var denseHash = list.Select((b, i) => new { b, i })
+                    .GroupBy(a => a.i / 16)
+                    .Select(g => g.Select(a => a.b).Aggregate((b1, b2) => (byte)(b1 ^ b2))).ToArray();
+
+                var knotHash = denseHash.Aggregate(new StringBuilder(), (sb, b) => sb.Append($"{b:x}".PadLeft(2, '0'))).ToString();
+                Console.WriteLine(knotHash);
             }
         }
+
+        
     }
 }
