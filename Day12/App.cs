e5180d6a53bec0af5449c723e1eb3105013eeb77
Ben Taylor <bentaylortheonly@gmail.com>
2017-12-12 21:05:46 +0000

    day12 part 1 passes tests

--- Day12/App.cs
+++ Day12/App.cs
@@ -17,8 +17,13 @@ namespace Day12
             while (!string.IsNullOrEmpty(line = Console.ReadLine()))
             {
                 var parts = line.Split(new[] {"<->", ", "}, StringSplitOptions.RemoveEmptyEntries);
-                var programs = parts.Select(int.Parse).Select(i => allPrograms.GetOrAdd(i, new Program(i))).ToArray();
-                programs.First().ConnectTo(programs.Skip(1));
+                var programs = parts
+                    .Select(int.Parse)
+                    .Select(i => allPrograms.GetOrAdd(i, new Program(i)))
+                    .OrderByDescending(p => p.Id)
+                    .ToArray();
+                programs.First().ConnectTo(
+                    programs.Concat(programs.SelectMany(p => p.Network())).Distinct().ToArray());
                 Console.WriteLine();
             }
             Console.WriteLine(allPrograms[0].ConnectedTo.Count);
@@ -35,19 +40,42 @@ namespace Day12
         {
             Id = id;
         }
+
+        public Program[] Network()
+        {
+            return ConnectedTo
+                .Concat(ConnectedTo.Where(c => c.Id < Id).SelectMany(c => c.Network()))
+                .Distinct().ToArray();
+        }
         
-        public void ConnectTo(IEnumerable<Program> others)
+        public void ConnectTo(Program[] others)
         {
             foreach (var program in others)
             {
-                program.ConnectedTo.Add(this);
                 ConnectedTo.Add(program);
+                program.ConnectedTo.Add(this);
+                if (program.Id < Id)
+                {
+                    program.ConnectTo(others);
+                }
             }
         }
 
         public override string ToString()
         {
-            return $"{Id} ({string.Join(", ", ConnectedTo)})";
+            return $"{Id} ({string.Join(", ", ConnectedTo.Select(c => c.Id.ToString()))})";
+        }
+
+        public override bool Equals(object obj)
+        {
+            var program = obj as Program;
+            return program != null &&
+                   Id == program.Id;
+        }
+
+        public override int GetHashCode()
+        {
+            return 2108858624 + Id.GetHashCode();
         }
     }
 }
