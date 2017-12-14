2c4fe8779839c91a8074e385a7229efbfa494fbb
Ben Taylor <bentaylortheonly@gmail.com>
2017-12-14 10:18:40 +0000

    genericize knothash

--- Day10/Program.cs
+++ Day10/Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
-using System.Linq;
-using System.Text;
+using Helpers;
 
 namespace Day10
 {
@@ -10,36 +9,11 @@ namespace Day10
         {
             string line;
 
-            int count = args.Length > 0 ? int.Parse(args[0]) : 256;
             while(!string.IsNullOrEmpty(line = Console.ReadLine()))
             {
-                byte[] list = Enumerable.Range(0, count).Select(i => (byte)i).ToArray();
-                int curPos = 0;
-                int skipSize = 0;
-                int[] lengths = line.Trim().Select(c => (int)c).Concat(new[] { 17, 31, 73, 47, 23 }).ToArray();
-                for (int round = 0; round < 64; round++)
-                {
-                    foreach (var length in lengths)
-                    {
-                        byte[] valuesToReverse = Enumerable.Range(0, length)
-                            .Select(i => list[(curPos + i) % list.Length]).Reverse().ToArray();
-                        Array.Copy(valuesToReverse, 0, list, curPos, Math.Min(length, list.Length - curPos));
-                        if(list.Length - curPos < length)
-                        {
-                            Array.Copy(valuesToReverse, list.Length - curPos, list, 0, length - (list.Length - curPos));
-                        }
-                        curPos = (curPos + (length + skipSize++)) % list.Length ;
-                    }
-                }
-                var denseHash = list.Select((b, i) => new { b, i })
-                    .GroupBy(a => a.i / 16)
-                    .Select(g => g.Select(a => a.b).Aggregate((b1, b2) => (byte)(b1 ^ b2))).ToArray();
-
-                var knotHash = denseHash.Aggregate(new StringBuilder(), (sb, b) => sb.Append($"{b:x}".PadLeft(2, '0'))).ToString();
+                var knotHash = Helper.GetKnotHash(line);
                 Console.WriteLine(knotHash);
             }
         }
-
-        
     }
 }
---
+++ Helpers.Tests/HelperTests.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+
+namespace Helpers.Tests
+{
+    [TestFixture]
+    public class HelperTests
+    {
+        [TestCase("", "a2582a3a0e66e6e86e3812dcb672a272")]
+        [TestCase("AoC 2017", "33efeb34ea91902bb2f59c9920caa6cd")]
+        [TestCase("1,2,3", "3efbe78a8d82f29979031a4aa0b16a9d")]
+        [TestCase("1,2,4", "63960835bcdc130f0b66d7ff4f6a5a8e")]
+        public void KnotHashTests(string input, string expectedKnotHash)
+        {
+            Assert.AreEqual(expectedKnotHash, Helper.GetKnotHash(input));
+        }
+    }
+}
